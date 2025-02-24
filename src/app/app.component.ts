40bd32e0038bdce1f28b6ebc839cdd18bfee1718
RustyGearBox <gearbox.bit@gmail.com>
2025-02-24 22:44:40 +0100

    Navbar inside Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
